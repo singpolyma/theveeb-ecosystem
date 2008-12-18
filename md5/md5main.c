e80921da052ac94f959f3d1d431c1e316c3ffd3d
Stephen Paul Weber <singpolyma@singpolyma.net>
2008-12-17 19:13:40 -0500

    Better byte => string algorithm. Credit: Dave Vandervies.

--- md5/md5main.c
+++ md5/md5main.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void md5(char * dst, char * string) {
+void md5(char * dst, char * src) {
+	/* dst must be at least 33 */
+	/* src can be any length */
 	int i;
+	int c = 0;
+	const char *hexdigits = "0123456789abcdef";
 	md5_state_t pms;
 	md5_byte_t digest[16];
 
@@ -11,20 +15,20 @@ void md5(char * dst, char * string) {
 	md5_init(&pms);
 
 	/* Append a string to the message. */
-	md5_append(&pms, (md5_byte_t*)string, strlen(string));
+	md5_append(&pms, (md5_byte_t*)src, strlen(src));
 
 	/* Finish the message and return the digest. */
 	md5_finish(&pms, digest);
 
-	dst[0] = '\0';
 	for(i = 0; i < 16; i++) {
-		sprintf(dst, "%s%x", dst, digest[i]);
+		dst[c++] = hexdigits[digest[i]>>4];
+		dst[c++] = hexdigits[digest[i]&0xf];
 	}
-	
+	dst[c++] = '\0';
 }
 
 int main(int argc, char *argv[]) {
-	char out[32];
+	char out[33]; /* MD5 is 32 + \0 */
 	if(argc < 2) {
 		fputs("You must specify a string to hash.\n", stderr);
 		return -1;
