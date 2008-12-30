c96c5f1edc28a080340daaacf713d4f8a14a05e8
Stephen Paul Weber <singpolyma@singpolyma.net>
2008-12-30 17:43:10 -0500

    TODO about -v

--- search/search.c
+++ search/search.c
@@ -56,6 +56,8 @@ int main (int argc, char ** argv) {
 	char * query = NULL;
 	int c;
 
+	/* TODO: support -v, show instead of list */
+
 	while((c = getopt(argc, argv, "-lhd:")) != -1) {
 		switch(c) {
 			case 'l':
