76c3f16772bb6c566d75fa694b5210fe7a111736
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-01-07 19:33:58 -0500

    select colour on click

--- gui.tcl
+++ gui.tcl
@@ -3,7 +3,7 @@ catch {package require tile}
 if [catch {ttk::setTheme tilegtk}] {
 	catch {ttk::setTheme tileqt}
 }
-#catch {namespace import -force ttk::*}
+catch {namespace import -force ttk::*}
 source scrollable.tcl
 
 # Get the main scrollable canvas
@@ -41,10 +41,11 @@ set pkgs [split [exec search/search ""] "\n"]
 puts $pkgs
 
 set i 0
+set highlightedrow 0
 foreach {item} $pkgs {
 	regexp {^(.)\s+(.+?)\s+(.+?)\s+(.+)$} $item matches status pkg version desc
-	frame ${canvas}.frame.row$i -highlightbackground "#abc" -highlightthickness 2
-	grid ${canvas}.frame.row$i -sticky nwe -row $i
+	canvas ${canvas}.frame.row$i -highlightbackground #abc -highlightthickness 0
+	grid ${canvas}.frame.row$i -pady 2 -sticky nwe -row $i
 
 	set cb [checkbutton ${canvas}.frame.row${i}.check -variable check$i]
 	set icon [canvas $canvas.frame.row$i.icon -height 24 -width 24 -background blue]
@@ -52,7 +53,11 @@ foreach {item} $pkgs {
 	set desc [label ${canvas}.frame.row$i.longer -text $desc -anchor w]
 
 	# Should get longer info from search eventually
-	set handler "$viewlabel configure -text $pkg"
+	set handler "$viewlabel configure -text $pkg
+	             ${canvas}.frame.row\$highlightedrow configure -highlightthickness 0
+	             set highlightedrow $i
+	             ${canvas}.frame.row$i configure -highlightthickness 2
+	             "
 	bind ${canvas}.frame.row$i <ButtonPress-1> $handler
 	bind $name <ButtonPress-1> $handler
 	bind $icon <ButtonPress-1> $handler
@@ -62,8 +67,8 @@ foreach {item} $pkgs {
 
 	grid $cb -column 0 -rowspan 2 -padx 5 -row $i
 	grid $icon -column 1 -rowspan 2 -padx 5 -row $i
-	grid $name -column 2 -padx 5 -sticky nwe -row $i
-	grid $desc -column 2 -padx 5 -sticky nwe -row [expr {1+$i}]
+	grid $name -column 2 -padx 5 -pady 2 -sticky nwe -row $i
+	grid $desc -column 2 -padx 5 -pady 2 -sticky nwe -row [expr {1+$i}]
 
 	grid columnconfigure ${canvas}.frame.row$i 2 -weight 1
 
