77b5359f790bc7e9525aceab19546d0c60775e74
Christopher Vollick <psycotica0@gmail.com>
2009-02-06 20:17:53 -0500

    Made the info box wrap.
    The long description in the package info now wraps.

--- gui.tcl
+++ gui.tcl
@@ -121,6 +121,9 @@ set description [frame ${tabArea}.description]
 set description.title [label ${description}.title -textvariable currentPackage(title) -font TkHeadingFont -justify left]
 set description.caption [label ${description}.caption -textvariable currentPackage(caption) -justify left]
 set description.longText [label ${description}.longText -textvariable currentPackage(longText) -justify left]
+
+bind . <Configure> [concat [list ${description.longText} configure -wraplength ] {[winfo width .]}]
+
 grid ${description.title} -sticky nw
 grid ${description.caption} -sticky nw
 grid ${description.longText} -sticky nwes
