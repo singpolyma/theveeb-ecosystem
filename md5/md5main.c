6e86f183dfa4fa19073894f8469542a795b4ff3b
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-02-07 16:38:15 -0500

    Allow for reading STDIN. Accepts binary data.

--- md5/md5main.c
+++ md5/md5main.c
@@ -1,8 +1,17 @@
 #include "md5.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void md5(char * dst, char * src) {
+#ifndef EXIT_SUCCESS
+	#define EXIT_SUCCESS 0
+#endif
+
+#ifndef EXIT_FAILURE
+	#define EXIT_FAILURE -1
+#endif
+
+void md5(char * dst, char * src, int len) {
 	/* dst must be at least 33 */
 	/* src can be any length */
 	int i;
@@ -15,7 +24,7 @@ void md5(char * dst, char * src) {
 	md5_init(&pms);
 
 	/* Append a string to the message. */
-	md5_append(&pms, (md5_byte_t*)src, strlen(src));
+	md5_append(&pms, (md5_byte_t*)src, len);
 
 	/* Finish the message and return the digest. */
 	md5_finish(&pms, digest);
@@ -29,11 +38,29 @@ void md5(char * dst, char * src) {
 
 int main(int argc, char *argv[]) {
 	char out[33]; /* MD5 is 32 + \0 */
-	if(argc < 2) {
-		fputs("You must specify a string to hash.\n", stderr);
-		return -1;
+	char * string = NULL;
+	int bufsize = 0;
+	int len = 0;
+	int c;
+	if(argc < 2 || (strlen(argv[1]) == 1 && argv[1][0] == '-')) {
+		/* Don't treat it as a string, because it may be binary data */
+		while((c = fgetc(stdin)) != EOF) {
+			if(bufsize == len) {
+				bufsize = 2*(bufsize+1)*sizeof(*string);
+				string = realloc(string, bufsize);
+				if(!string) {
+					fputs("Failed to allocate memory\n", stderr);
+					exit(EXIT_FAILURE);
+				}
+			}
+			string[len] = (char)c;
+			len++;
+		}
+	} else {
+		string = argv[1];
+		len = strlen(string);
 	}
-	md5(out, argv[1]);
+	md5(out, string, len);
 	printf("%s\n", out);
-	return 0;
+	exit(EXIT_SUCCESS);
 }
