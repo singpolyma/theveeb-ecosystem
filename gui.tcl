95889778d018c6ad223b06aef760417c9320aedd
Christopher Vollick <psycotica0@gmail.com>
2009-02-10 09:42:14 -0500

    Fixed vertical scaling, again.
    Every time I add a row to the UI, I need to remember to change which rows I want to scale and which I want to remain static.
    
    This time, the category box was being crushed first.

--- gui.tcl
+++ gui.tcl
@@ -179,8 +179,8 @@ grid $viewarea -sticky news
 grid .viewyscroll -sticky ns
 
 # Make grid fill window
-grid rowconfigure . 1 -weight 1
 grid rowconfigure . 2 -weight 1
+grid rowconfigure . 3 -weight 1
 grid columnconfigure . 0 -weight 1
 
 # And make rows fill canvas
