8035ceda42d4619313ebabf7c2b955ac58a20126
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-02-21 23:30:18 -0500

    remove script
    Wrote remove script in sh
    Tries to remove using external package manager if present.
    If there is an install log generated by undeb in the right place, it
    removes the files referenced by that, and then removed the log itself.

--- remove.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-# TODO: add ability to redownload deb file and remove specific files... ala undeb but in reverse
-
-`sudo dpkg -r "#{ARGV[0]}"`
---
+++ remove.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# Tell zsh we expect to be treated like an sh script
+# zsh really should take the hint from the shebang line
+if which emulate 1>&2; then
+	emulate sh
+fi
+
+# Make sure HOME is set up
+if [ -z "$HOME" ]; then
+	HOME="`ls -d ~`"
+fi
+
+INTERACTIVE=0
+while [ $# -gt 1 ]; do
+	case "$1" in
+		-d*)
+			TVEDB="`echo "$1" | cut -c3-`"
+			if [ -z "$TVEDB" ]; then
+				TVEDB=">>"
+			fi
+			shift
+		;;
+		-*)
+			echo "Unsupported switch $1" 1>&2
+			exit 1
+		;;
+		*)
+			if [ "$TVEDB" = ">>" ]; then
+				TVEDB="$1"
+				shift
+			fi
+		;;
+	esac
+done
+
+# Check that a package was specified to remove
+if [ -z "$1" ]; then
+	echo "No package was specified." 1>&2
+	exit 1
+fi
+
+# Auto-select install prefix
+if [ -z "$PREFIX" ]; then
+	PREFIX="$TVEROOT/"
+fi
+
+# Select directory for install logs
+LOGDIR=""
+if [ -d "$TVEROOT/Library/Caches" ]; then
+	LOGDIR="$TVEROOT/Library/Caches/tve-remove"
+else
+	LOGDIR="$TVEROOT/var/cache/tve-remove"
+fi
+if ! mkdir -p "$LOGDIR"; then
+	LOGDIR="$HOME/.tve-remove"
+	if ! mkdir -p "$LOGDIR"; then
+		echo "Could not access the install log directory." 1>&2
+		exit 1
+	fi
+fi
+
+# Determine which command to use for installing internal packages
+INTERNAL="`which dpkg`"
+if [ $? != 0 -o "`whoami`" != "root" ]; then
+	INTERNAL="undeb"
+else
+	INTERNAL="dpkg --root="$TVEROOT/" -r"
+fi
+
+if [ "$INTERNAL" != "undeb" ]; then
+	if ! $INTERNAL "$1"; then
+		echo "Removal failed." 1>&2
+		exit 1
+	fi
+fi
+
+if [ -r "$LOGDIR/$1" ]; then
+	if ! xargs rm -fv < "$LOGDIR/$1"; then
+		echo "Removal failed." 1>&2
+		exit 1
+	fi
+	if ! rm -fv "$LOGDIR/$1"; then
+		echo "Removal failed." 1>&2
+		exit 1
+	fi
+elif [ "$INTERNAL" = "undeb" ]; then
+	echo "$1 not installed." 1>&2
+	exit 1
+fi
+
+echo "$1 sucessfully removed."
