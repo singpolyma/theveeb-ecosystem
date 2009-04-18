e255cd04fa2230373d84b31397b4c35dca341cc7
Christopher Vollick <psycotica0@gmail.com>
2009-04-18 09:40:56 -0400

    Made Login more Robust
    Added checks to find which commands to use.
    Added checks to make sure requests succeeded.

--- login-finish.sh
+++ login-finish.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
-REQUEST="`oauthsign -c key123 -C sekret -t $1 -T $2 http://csclub.uwaterloo.ca:4567/oauth/access_token`"
-TOKENS="`curl -s "$REQUEST"`"
+# Verify tokens were passed in
+if [ "$#" -ne 2 ]; then
+	echo "Expected two tokens" 1>&2
+	echo "USAGE: login-finish TOKEN SECRET" 1>&2
+	exit 1
+fi
+
+# Find the network utility
+if command -v wget 1>&2; then
+	GET="wget -q -O -"
+elif command -v curl 1>&2; then
+	GET="curl -sfL"
+else
+	echo "You must have wget or curl installed." 1>&2
+	exit 1
+fi
+
+# Verify the presence of oauthsign
+if ! command -v oauthsign 1>&2; then
+	echo "You need the oauthsign utility from oauth-utils installed to use this script." 1>&2
+	exit 1
+fi
+
+REQUEST="`oauthsign -c key123 -C sekret -t "$1" -T "$2" http://csclub.uwaterloo.ca:4567/oauth/access_token`"
+TOKENS="`$GET "$REQUEST"`"
+
+# Verify the expected output was returned
+if [ "`echo $TOKENS | cut -c 1-11`" != "oauth_token" ]; then
+	# Request Failed
+	echo "Authentication failed on server." 1>&2
+	echo "The server could be unreachable, invalid tokens were given to this script, or you could have failed to authorize this application." 1>&2
+	exit 1
+fi
 
 TOKEN="`echo $TOKENS | sed 's/^oauth_token=\([^&]*\).*/\1/'`"
 SECRET="`echo $TOKENS | sed 's/^[^&]*&oauth_token_secret=\(.*\)/\1/'`"
--- login-start.sh
+++ login-start.sh
@@ -1,10 +1,56 @@
 #!/bin/sh
 
+# Find the network utility
+if command -v wget 1>&2; then
+	GET="wget -q -O -"
+elif command -v curl 1>&2; then
+	GET="curl -sfL"
+else
+	echo "You must have wget or curl installed." 1>&2
+	exit 1
+fi
+
+# Verify the presence of oauthsign
+if ! command -v oauthsign 1>&2; then
+	echo "You need the oauthsign utility from oauth-utils installed to use this script." 1>&2
+	exit 1
+fi
+
+# Find the command to open a URL, if there is one
+if command -v xdg-open 1>&2; then
+	OPEN="xdg-open"
+elif command -v open 1>&2; then
+	OPEN="open"
+elif command -v run 1>&2; then
+	OPEN="run"
+else 
+	OPEN=""
+fi
+
 REQUEST="`oauthsign -c key123 -C sekret http://csclub.uwaterloo.ca:4567/oauth/request_token`"
-TOKENS="`curl -s "$REQUEST"`"
+TOKENS="`$GET "$REQUEST"`"
+
+# Verify the expected output was returned
+if [ "`echo $TOKENS | cut -c 1-11`" != "oauth_token" ]; then
+	# Request Failed
+	echo "Authentication failed on server." 1>&2
+	echo "The server could be unreachable, invalid tokens were given to this script, or you could have failed to authorize this application." 1>&2
+	exit 1
+fi
 
 TOKEN="`echo $TOKENS | sed 's/^oauth_token=\([^&]*\).*/\1/'`"
 SECRET="`echo $TOKENS | sed 's/^[^&]*&oauth_token_secret=\(.*\)/\1/'`"
 
+# This is the url to send the user to
+URL="http://singpolyma.net/theveeb/authorize.php?oauth_token=$TOKEN" 
+
+# Output the tokens
 echo "$TOKEN $SECRET"
-open "http://singpolyma.net/theveeb/authorize.php?oauth_token=$TOKEN" &
+
+if [ -n "$OPEN" ]; then
+	# If we can open urls for the user, do so
+	$OPEN "$URL" &
+else
+	# If we can't, output the url they should go to on their own
+	echo "Go to \"$URL\" in a browser to continue authentication." 1>&2
+fi
