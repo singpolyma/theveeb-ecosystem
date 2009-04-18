d53460e1913cd058a4dbd155b5acfcc1a8d3871e
Christopher Vollick <psycotica0@gmail.com>
2009-04-17 21:52:07 -0400

    Basic Implementation of Login.
    Works On My System.
    Does no error handling.
    Assumes everything works.
    Assumes you actually authenticate between login-start.sh and login-finish.sh.

--- login-finish.sh
+++ login-finish.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-#oauthsign -c key123 -C sekret -t TOKEN -T SECRET http://csclub.uwaterloo.ca:4567/oauth/access_token
-# parse tokens, save into file or output
+REQUEST="`oauthsign -c key123 -C sekret -t $1 -T $2 http://csclub.uwaterloo.ca:4567/oauth/access_token`"
+TOKENS="`curl -s "$REQUEST"`"
+
+TOKEN="`echo $TOKENS | sed 's/^oauth_token=\([^&]*\).*/\1/'`"
+SECRET="`echo $TOKENS | sed 's/^[^&]*&oauth_token_secret=\(.*\)/\1/'`"
+
+echo "$TOKEN $SECRET" > "$HOME/.tve-oauth-tokens"
--- login-start.sh
+++ login-start.sh
@@ -2,5 +2,9 @@
 
 REQUEST="`oauthsign -c key123 -C sekret http://csclub.uwaterloo.ca:4567/oauth/request_token`"
 TOKENS="`curl -s "$REQUEST"`"
-# TODO: parse token/secret
-#http://singpolyma.net/theveeb/authorize.php?oauth_token=TOKEN
+
+TOKEN="`echo $TOKENS | sed 's/^oauth_token=\([^&]*\).*/\1/'`"
+SECRET="`echo $TOKENS | sed 's/^[^&]*&oauth_token_secret=\(.*\)/\1/'`"
+
+echo "$TOKEN $SECRET"
+open "http://singpolyma.net/theveeb/authorize.php?oauth_token=$TOKEN" &
