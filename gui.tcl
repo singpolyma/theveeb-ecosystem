a013a9f770c8dc50235f09d86b9cbb54dde49ed6
Christopher Vollick <psycotica0@gmail.com>
2009-08-01 12:39:03 -0400

    Find calculatePrice with findTVEScript
    Rather than assuming it's in the same directory as the GUI.

--- gui.tcl
+++ gui.tcl
@@ -287,7 +287,7 @@ proc getCost {diff} {
 		return 0
 	}
 
-	return [eval [join [concat exec sh calculateTotal.sh $apps] { }]]
+	return [eval [join [concat exec sh [findTVEscript calculateTotal] $apps] { }]]
 }
 
 # This is the command of the "Do It" button
