0621ae70b92e5fee75218f8dd275a1d8e936c5b2
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-11-27 12:41:22 -0500

    Need tve-setup to fix Cygwin

--- logout.sh
+++ logout.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -r "`dirname "$0"`"/tve-setup.sh ]; then
+	. "`dirname "$0"`"/tve-setup.sh
+else
+	. "$TVEROOT"/usr/lib/tve-setup.sh
+fi
+
 # Find the file where OAuth tokens are and get them
 OAUTHTOKENS="$HOME/.tve-oauth-tokens"
 if [ ! -r "$OAUTHTOKENS" ]; then
